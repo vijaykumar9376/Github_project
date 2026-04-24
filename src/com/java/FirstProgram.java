39a7c7b10108efb224fe120d5445499c964cb2be
vijaykumar9376 <vijaygoudajambagi.1@gmail.com>
2026-04-24 11:39:48 +0530

    My first commit

---
+++ src/com/java/FirstProgram.java
@@ -0,0 +1,8 @@
+package com.java;
+
+public class FirstProgram {
+
+	void print() {
+		System.out.print("Welcome back to github session2");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Github_Project {
+}
